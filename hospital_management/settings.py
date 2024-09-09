e48f07918f0edfda0ce4286726cd7bc43adbc0ac
swayam <swayam@gmail.com>
2024-09-09 10:55:58 +0530

    error

--- hospital_management/settings.py
+++ hospital_management/settings.py
@@ -118,6 +118,8 @@ CSRF_TRUSTED_ORIGINS = [
     'https://your-domain.com',
 ]
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static'),
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATIC_URL = '/staticfiles/'
